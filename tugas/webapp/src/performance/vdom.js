ece082cfc68d2d42f9ac13a10a06dc4c8f4e4d2f
Muhammad Choirudin Malik <77602202+choirmalik@users.noreply.github.com>
2021-02-16 15:50:49 +0700

    ubah performance webapp menggunakan virtual dom

--- tugas/webapp/src/performance/vdom.js
+++ tugas/webapp/src/performance/vdom.js
@@ -1,20 +1,41 @@
 require('./main.css');
-const Vue = require('vue');
+const Vue = require('vue').default;
 const { store$ } = require('./store');
+const { summary } = require('./async-action');
+const { NavComp } = require('./components/nav');
+const { StateComp } = require('./components/state-div');
+const { PerfComp } = require('./components/performance-list');
 
 new Vue({
-  el: '#app1',
+  el: '#performance-app',
+  components: {
+    navigation: NavComp,
+    'state-page': StateComp,
+    'performance-list': PerfComp,
+  },
   render(createElement) {
-    return createElement('div', 'initial');
+    return createElement('div', [
+      createElement('navigation'),
+      createElement('state-page', {
+        props: {
+          errMsg: this.state.error,
+          load: this.state.loading,
+        },
+      }),
+      createElement('performance-list', {
+        props: { summary: this.state.summary },
+      }),
+    ]);
   },
   data: {
-    init: '',
+    state: {},
   },
 
   mounted() {
-    this.init = store$.getState();
+    this.state = store$.getState();
     store$.subscribe(() => {
-      this.init = store$.getState();
+      this.state = store$.getState();
     });
+    store$.dispatch(summary);
   },
 });
