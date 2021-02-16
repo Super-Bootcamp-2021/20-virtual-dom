94085eb9f4eca2bf077651d252079c905b34524e
Muhammad Hasan <hasan@sirus.dev>
2021-02-16 08:15:57 +0700

    bootstrap base project

---
+++ materi/src/lib/todo/app.ts
@@ -0,0 +1,70 @@
+import './app.css';
+import { Task } from './reducer';
+import { store$ } from './store';
+import {
+  addTaskAsync,
+  loadTasksAsync,
+  doneTaskAsync,
+  undoneTaskAsync,
+} from './todo-client';
+
+// view
+const input = document.getElementById('todo');
+const form = document.getElementById('todo-form');
+const list = document.getElementById('todo-list');
+
+if (form && input) {
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    const task = input.nodeValue;
+    if (!task?.length) {
+      return;
+    }
+    // dispatch action add
+    store$.dispatch<any>(addTaskAsync(task));
+    input.nodeValue = '';
+  };
+} else {
+  // handle error
+}
+
+// presentation layer
+store$.subscribe(() => {
+  const state = store$.getState();
+  render(state);
+});
+const state = store$.getState();
+render(state);
+
+store$.dispatch<any>(loadTasksAsync);
+
+/**
+ * render application
+ * @param state state of application
+ */
+function render(state: Task[]) {
+  if (!list) {
+    // handle error
+    return;
+  }
+  list.innerHTML = '';
+  for (let i = 0; i < state.length; i++) {
+    const todo = state[i];
+    const li = document.createElement('li');
+    li.textContent = todo.task;
+    if (todo.done) {
+      li.className = 'todo-done';
+      li.onclick = function () {
+        // dispatch action done
+        store$.dispatch<any>(undoneTaskAsync(todo.id));
+      };
+    } else {
+      li.className = '';
+      li.onclick = function () {
+        // dispatch action done
+        store$.dispatch<any>(doneTaskAsync(todo.id));
+      };
+    }
+    list.append(li);
+  }
+}
