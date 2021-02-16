b8d6f29d9a8ee758d020cce80297ef2c289889f1
dimas1997 <dimas6597@gmail.com>
2021-02-16 17:19:04 +0700

    change webapp performance to virtual dom

--- tugas/webapp/src/performance/main.ts
+++ tugas/webapp/src/performance/main.ts
@@ -1,59 +1,57 @@
-import { summary } from './async-action';
+import Vue, { CreateElement, VNode } from 'vue';
+import { navigation } from './components/navigation';
+import { loadingMessage, errorMessage } from './components/message';
+import { summary } from './components/performance';
 import { store$ } from './store';
-import {State} from './reducer';
-
+import { summary as summaryAction } from './async-action';
 import './main.css';
-
-const workers = document.getElementById('workers');
-const tasks = document.getElementById('tasks');
-const done = document.getElementById('task-done');
-const canceled = document.getElementById('task-canceled');
-const refresh = document.getElementById('refresh');
-const errorTxt = document.getElementById('error-text');
-const loadingTxt = document.getElementById('loading-text');
-
-// presentation layer
-store$.subscribe(() => {
-  const state = store$.getState();
-  render(state);
+const buttonRefresh = Vue.extend({
+  render(createElement: CreateElement): VNode {
+    return createElement('button', {
+      attrs: {
+        id: 'refresh',
+      },
+      domProps: {
+        innerHTML: 'refresh',
+      },
+      on: {
+        click: (event) => {
+          store$.dispatch<any>(summaryAction);
+        },
+      },
+    });
+  },
 });
-const state = store$.getState();
-render(state);
 
-store$.dispatch<any>(summary);
-
-if(refresh){
-  refresh.onclick = () => {
-    store$.dispatch<any>(summary);
-  };
-}
-
-function render(state: State): void {
-  if(errorTxt) {
-    // render error
-    if (state.error) {
-      errorTxt.textContent = state.error.toString();
-    } else {
-      errorTxt.textContent = '';
-    }
-  }
-
-  
-    if (state.loading) {
-      if(loadingTxt){
-        loadingTxt.style.removeProperty('display');
-      }
-    } else {
-      if(loadingTxt){
-        loadingTxt.style.display = 'none';
-      }
-    }
-
-  if(workers && tasks && done && canceled){
-    workers.innerText = state.summary.total_worker.toString();
-    tasks.innerText = state.summary.total_task.toString();
-    done.innerText = state.summary.task_done.toString();
-    canceled.innerText = state.summary.task_cancelled.toString();
-  }
-  // render list of worker
-}
+const app1 = new Vue({
+  el: '#performance',
+  components: {
+    navigation: navigation,
+    loadingMessage: loadingMessage,
+    errorMessage: errorMessage,
+    buttonRefresh: buttonRefresh,
+    performSummary: summary,
+  },
+  render(h: CreateElement): VNode {
+    return h('div', [
+      h(navigation),
+      h('hr'),
+      h(loadingMessage, { props: { loading: this.performance.loading } }),
+      h(errorMessage, { props: { error: this.performance.error } }),
+      h(buttonRefresh),
+      h(summary, { props: { details: this.performance.summary } }),
+    ]);
+  },
+  data: {
+    performance: {},
+  },
+  created() {
+    this.performance = store$.getState();
+  },
+  mounted() {
+    store$.subscribe(() => {
+      this.performance = store$.getState();
+    });
+    store$.dispatch<any>(summaryAction);
+  },
+});
