3bc39c594d8c50ac9bceac1209b38b7579c5d65d
Muhammad Achsan Hujjatul Islam <achsanh@gmail.com>
2021-02-16 18:01:42 -0500

    [Checkpoint 1] Convert Webapp to VDOM was done

--- tugas/webapp/src/tasks/components/assignee-select.ts
+++ tugas/webapp/src/tasks/components/assignee-select.ts
@@ -1,36 +1,38 @@
 import Vue, { CreateElement, VNode } from 'vue';
 import { Worker } from '../reducer';
-import { store$ } from '../store';
 
 export const AssigneeSelect = Vue.extend({
   props: ['workers'],
   render(createElement: CreateElement): VNode {
-    let temp = [
-      {
-        id: 0,
-        name: 'Name',
-      },
-      {
-        id: 1,
-        name: 'Budi',
-      },
-      {
-        id: 2,
-        name: 'Susi',
-      },
-    ];
-    // const workerList = this.$props.workers.map((worker: Worker) => {
-    const workerList = temp.map((worker) => {
-      return createElement(
-        'option',
-        {
-          attrs: {
-            value: worker.id,
+    // let temp = [
+    //   {
+    //     id: 0,
+    //     name: 'Name',
+    //   },
+    //   {
+    //     id: 1,
+    //     name: 'Budi',
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Susi',
+    //   },
+    // ];
+    // const workerList = temp.map((worker) => {
+    var workerList = [];
+    if (this.$props.workers) {
+      workerList = this.$props.workers.map((worker: Worker) => {
+        return createElement(
+          'option',
+          {
+            attrs: {
+              value: worker.id,
+            },
           },
-        },
-        worker.name
-      );
-    });
+          worker.name
+        );
+      });
+    }
     var self = this;
     return createElement(
       'select',
@@ -39,17 +41,30 @@ export const AssigneeSelect = Vue.extend({
           name: 'assignee',
           id: 'assignee',
         },
-        domProps: {
-          // Untuk init value of select
-          value: self.value,
-        },
+        // domProps: {
+        //   // Untuk init value of select
+        //   value: self.value,
+        // },
         on: {
           input: function (event) {
-            self.value = event.target.value;
+            // self.value = event.target.value;
+            self.$emit('test', event);
           },
         },
       },
-      [workerList]
+      [
+        // Not Best Practice
+        createElement(
+          'option',
+          {
+            attrs: {
+              value: '',
+            },
+          },
+          ''
+        ),
+        workerList,
+      ]
     );
   },
 });
--- tugas/webapp/src/tasks/components/task-list.ts
+++ tugas/webapp/src/tasks/components/task-list.ts
@@ -1,16 +1,62 @@
 import Vue, { CreateElement, VNode } from 'vue';
 import { Task } from '../reducer';
+import { done, cancel } from '../async-action';
 import { store$ } from '../store';
 
 export const TaskList = Vue.extend({
   props: ['tasks'],
   render(createElement: CreateElement): VNode {
-    const taskList = this.$props.tasks.map((task: Task) => {
-      return createElement(
-        'li',
-        task.task
-      );
-    });
+    var taskList = [];
+    if (this.$props.tasks) {
+      taskList = this.$props.tasks.map((task: Task) => {
+        return createElement('li', [
+          createElement('div', [
+            createElement('a', task.attachment),
+            createElement('span', task.job),
+            createElement('span', task.assignee),
+            task.done
+              ? createElement('span', 'sudah selesai')
+              : [
+                  createElement(
+                    'button',
+                    {
+                      on: {
+                        // click: this.doneBtn(task.id),
+                        click: () => {
+                          store$.dispatch<any>(done(task.id));
+                        },
+                      },
+                    },
+                    'selesai'
+                  ),
+                  createElement(
+                    'button',
+                    {
+                      on: {
+                        // click: this.cancelBtn(task.id),
+                        click: () => {
+                          store$.dispatch<any>(cancel(task.id));
+                        },
+                      },
+                    },
+                    'batal'
+                  ),
+                ],
+          ]),
+        ]);
+      });
+    }
     return createElement('ol', taskList);
+  },
+  methods: {
+    // Kok Error
+    doneBtn(id) {
+      console.log(id);
+      // store$.dispatch<any>(done(id));
+    },
+    cancelBtn(id) {
+      console.log(id);
+      // store$.dispatch<any>(cancel(id));
+    },
   },
 });
