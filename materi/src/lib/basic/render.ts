48fb8bb381608ffd917923296ecda560931937ec
Muhammad Hasan <hasan@sirus.dev>
2021-02-16 10:20:34 +0700

    add basic virtual dom example

---
+++ materi/src/lib/basic/render.ts
@@ -0,0 +1,80 @@
+import Vue, { CreateElement, VNode } from 'vue';
+
+const app1 = new Vue({
+  el: '#app-1',
+  render(createElement: CreateElement): VNode {
+    return createElement('div', [
+      createElement('input', {
+        domProps: {
+          value: this.message,
+        },
+        on: {
+          input: (event) => {
+            this.message = event.target.value;
+          },
+        },
+      }),
+      this.hide ? null : createElement('p', this.message),
+      createElement(
+        'button',
+        {
+          on: {
+            click: this.reverse,
+          },
+        },
+        'kebalik'
+      ),
+      createElement(
+        'button',
+        {
+          on: {
+            click: this.toggleHide,
+          },
+        },
+        this.hide ? 'lihat' : 'tutup'
+      ),
+    ]);
+  },
+  data: {
+    message: 'selamat pagi',
+    hide: false,
+  },
+  methods: {
+    reverse() {
+      this.message = this.message.split('').reverse().join('');
+    },
+    toggleHide() {
+      this.hide = !this.hide;
+    },
+  },
+});
+
+const app2 = new Vue({
+  el: '#app-2',
+  render(createElement: CreateElement): VNode {
+    const todos: VNode[] = [];
+    for (const todo of this.todos) {
+      todos.push(createElement('li', { style: { color: 'red' } }, todo.text));
+    }
+    return createElement('ul', todos);
+  },
+  data: {
+    todos: [
+      { text: 'makan' },
+      { text: 'minum' },
+      { text: 'belajar' },
+      { text: 'main' },
+    ],
+  },
+  beforeCreate() {
+    console.log('before create');
+  },
+  beforeUpdate() {
+    console.log('will render DOM');
+  },
+  mounted() {
+    for (let i = 0; i < 99; i++) {
+      this.todos.push({ text: 'teks ke ' + i });
+    }
+  },
+});
--- materi/webpack.config.js
+++ materi/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: {
     main: './src/main.ts',
-    schema: './schema/main.ts',
+    basic: './src/lib/basic/main.ts'
   },
   output: {
     path: path.resolve(__dirname, 'www'),
