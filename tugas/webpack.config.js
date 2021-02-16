10900185d2dcf98344b5709a53f8f1b018a9fcb0
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-02-16 12:26:26 +0700

    add init configuration

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -1,3 +1,4 @@
+const DotenvWebpackPlugin = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
@@ -15,11 +16,29 @@ module.exports = {
     contentBase: './webapp//www',
     port: 7000,
   },
+  plugins: [
+    new DotenvWebpackPlugin({
+      path: './.env',
+      safe: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.yaml$/,
+        use: [{ loader: 'json-loader' }, { loader: 'yaml-loader' }],
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
